7bbecf0159fd4320f6a84928c95d73781b289d81
Romain Crestey <rcrestey@gmail.com>
2020-02-14 14:18:26 +0100

    refactor: use class component

--- parole/src/App.js
+++ parole/src/App.js
@@ -4,28 +4,32 @@ import { Line } from 'react-chartjs-2';
 
 import './App.css';
 
-function App({ data, types }) {
-  const datasets = Object.entries(data).map(([name, value]) => ({
-    label: name,
-    data: value.wers,
-    borderColor: randomColor(),
-    pointRadius: 2,
-    fill: false,
-  }));
+class App extends React.Component {
+  render() {
+    const { data, types } = this.props;
 
-  return (
-    <div className='App'>
-      <Line
-        data={{
-          labels: data['France Culture'].years,
-          datasets,
-        }}
-        options={{
-          legend: { display: true },
-        }}
-      />
-    </div>
-  );
+    const datasets = Object.entries(data).map(([name, value]) => ({
+      label: name,
+      data: value.wers,
+      borderColor: randomColor(),
+      pointRadius: 2,
+      fill: false,
+    }));
+
+    return (
+      <div className='App'>
+        <Line
+          data={{
+            labels: data['France Culture'].years,
+            datasets,
+          }}
+          options={{
+            legend: { display: true },
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
